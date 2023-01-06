4df035f9fd122755405186d473e837e222c9834f
wshi01 <jayceengineer2@gmail.com>
2023-01-06 09:19:11 -0500

    commented important parts of code

--- lib/home/body.dart
+++ lib/home/body.dart
@@ -14,67 +14,72 @@ class _bodyViewState extends State<bodyView> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 2400,
-        child: ListView.builder(
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: 5,
-            itemBuilder: (context, index) {
-              return Column(
+      // sizing depends on size of the elements it contains, should be made dynamic later
+      height: 2400,
+      child: ListView.builder(
+        // NeverScrollableScrollPhysics makes scrolling the whole screen possible instead of the container only scrollable
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  margin: EdgeInsets.only(top: 20, left: 30, bottom: 10),
+                  child: const Text(
+                    'Sketch of a girl',
+                    style: TextStyle(fontSize: 35, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  margin: EdgeInsets.only(left: 30, bottom: 10),
+                  child: const Text(
+                    'Monday October 24, 2022',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
+              Stack(
+                // stack allows the containers or other widgets to be stacked on top of each other
                 children: [
                   Align(
-                    alignment: Alignment.topLeft,
+                    // outer blue container
+                    alignment: Alignment.center,
                     child: Container(
-                      margin: EdgeInsets.only(top: 20, left: 30, bottom: 10),
-                      child: const Text(
-                        'Sketch of a girl',
-                        style: TextStyle(
-                            fontSize: 35, fontWeight: FontWeight.w500),
-                      ),
+                      width: 370,
+                      height: 370,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(25),
+                          color: Colors.blue),
                     ),
                   ),
                   Align(
-                    alignment: Alignment.topLeft,
+                    // container that contains the image
+                    alignment: Alignment.center,
                     child: Container(
-                      margin: EdgeInsets.only(left: 30, bottom: 10),
-                      child: const Text(
-                        'Monday October 24, 2022',
-                        style: TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.w500),
+                      width: 350,
+                      height: 350,
+                      margin: EdgeInsets.only(top: 10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25),
+                        color: Colors.white,
+                        image: const DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage("figures/girlimage1.png"),
+                        ),
                       ),
                     ),
                   ),
-                  Stack(
-                    children: [
-                      Align(
-                        alignment: Alignment.center,
-                        child: Container(
-                          width: 370,
-                          height: 370,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(25),
-                              color: Colors.blue),
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.center,
-                        child: Container(
-                          width: 350,
-                          height: 350,
-                          margin: EdgeInsets.only(top: 10),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(25),
-                            color: Colors.white,
-                            image: const DecorationImage(
-                              fit: BoxFit.cover,
-                              image: AssetImage("figures/girlimage1.png"),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
                 ],
-              );
-            }));
+              ),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
--- lib/util/colors.dart
+++ lib/util/colors.dart
@@ -1,5 +1,10 @@
 import 'dart:ui';
 
 class CustomColors {
+  // can add colors here
+  // try if you can use this:
+  // static final Color testColor = Color.fromHex('#0f0f0f');
+  // for me it does not work ~ Willie
+
   static final Color mainpagebgcolor = Color.fromARGB(240, 240, 240, 240);
 }
