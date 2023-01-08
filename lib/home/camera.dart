ee8b2083be67cbdb52d40024fdefa245c52b36b5
wshi01 <jayceengineer2@gmail.com>
2023-01-08 15:23:30 -0500

    Camera added camera support still need to display image taken via camera

--- lib/home/camera.dart
+++ lib/home/camera.dart
@@ -1,8 +1,7 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-
-import '../util/colors.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:async';
 
 class CameraPage extends StatefulWidget {
   const CameraPage({super.key});
@@ -12,11 +11,47 @@ class CameraPage extends StatefulWidget {
 }
 
 class _CameraPageState extends State<CameraPage> {
+  File? _image;
+  final imagePicker = ImagePicker();
+
+  Future _getImage() async {
+    File picture =
+        (await imagePicker.pickImage(source: ImageSource.camera)) as File;
+    if (picture == null) {
+      return;
+    } else {
+      File? image = File(picture.path);
+      setState(() {
+        _image = image;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: CustomColors.mainpagebgcolor,
-      body: Container(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          margin: const EdgeInsets.only(top: 400),
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(bottom: 300),
+                child: Center(
+                  child: _image == null
+                      ? const Text('No image yet')
+                      : Image.file(_image!),
+                ),
+              ),
+              FloatingActionButton(
+                onPressed: _getImage,
+                child: const Icon(Icons.camera_alt),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
