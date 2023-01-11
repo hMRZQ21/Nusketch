7a3c3bd8e5d1893d3d9b3644a8353beb18fa34f2
wshi01 <jayceengineer2@gmail.com>
2023-01-10 21:12:10 -0500

    added link to another page for images on home page

---
+++ lib/home/description.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class DescriptionPage extends StatefulWidget {
+  const DescriptionPage({super.key});
+
+  @override
+  State<DescriptionPage> createState() => _DescriptionPageState();
+}
+
+class _DescriptionPageState extends State<DescriptionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      color: Colors.green,
+    );
+  }
+}
