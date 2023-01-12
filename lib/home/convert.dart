92fc483ae73e116f409a643c9284da21b99a6ca9
wshi01 <jayceengineer2@gmail.com>
2023-01-11 21:55:45 -0500

    Camera and description added some theory code for converting image to sketch, still need to find out how to output a ui.Image for camera added more parts of the description page need to find out how to hide the top status bar so no need to have top margins back button works

---
+++ lib/home/convert.dart
@@ -0,0 +1,46 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'dart:ui' as ui;
+
+class ConvertSketch extends StatelessWidget {
+  final ui.Image image;
+
+  const ConvertSketch({super.key, required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return RepaintBoundary(
+      child: CustomPaint(
+        painter: _Converting(image),
+      ),
+    );
+  }
+}
+
+class _Converting extends CustomPainter {
+  final ui.Image image;
+  _Converting(this.image);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final pictureRecorder = PictureRecorder();
+    final recorderCanvas = Canvas(pictureRecorder);
+
+    recorderCanvas.drawImage(image, Offset.zero, Paint());
+    recorderCanvas.drawImage(image, Offset.zero,
+        Paint()..imageFilter = ImageFilter.blur(sigmaX: 5, sigmaY: 5));
+    // need to create custom class for grayscale.
+    // recorderCanvas.drawImage(
+    //     image, Offset.zero, Paint()..imageFilter = ImageFilter.grayscale);
+    final picture = pictureRecorder.endRecording();
+    canvas.drawPicture(picture);
+  }
+
+  @override
+  bool shouldRepaint(_Converting oldDelegate) {
+    return image != oldDelegate.image;
+  }
+}
