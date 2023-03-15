674c198b7eff5d5051e92b33db6393679d0941bf
JessieSu2 <JessieSu3@gmail.com>
2023-03-14 22:24:09 -0400

    adding converting loading screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:nusketch/loading.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:nusketch/pages/convertingloading.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Loading(),
+      home: ConvertLoadingScreen(),//const Loading(),
     );
   }
 }
