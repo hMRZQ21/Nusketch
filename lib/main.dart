e595555247e2c746cc27747ce9fac78eab224a90
JessieSu2 <JessieSu3@gmail.com>
2023-03-14 22:25:00 -0400

    changed home route to Loading() instead of ConvertLoading()

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ConvertLoadingScreen(),//const Loading(),
+      home: const Loading(),
     );
   }
 }
